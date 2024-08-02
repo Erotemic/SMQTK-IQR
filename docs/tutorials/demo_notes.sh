97f20110b7d2b0f1016429b240ba60af68c1049c
joncrall <erotemic@gmail.com>
2024-08-02 17:16:50 -0400

    Fixup tutorial notes

--- docs/tutorials/demo_notes.sh
+++ docs/tutorials/demo_notes.sh
@@ -2,12 +2,12 @@
 # IQR demo with PrePopulated Descriptor Set
 # ---------------------------------------------------------------------------
 
-"""
+__doc__='
 May 27, 2024
 The following guide provides steps to run the IQR demo with a prepopulated
 descriptor set. The demo was written and run with Python version 3.11.2.
 Older versions of packages flask 2.0.1 and werkzeug 2.0.0 were used.
-The demo uses two werkzeug method 'pop_path_info' and 'peek_path_info' that
+The demo uses two werkzeug method `pop_path_info` and `peek_path_info` that
 were removed in werkzeug 2.3.0 release that were deprecated in 2.2.0. These
 methods are used by the IQR Search Dispatcher that generates the link for
 each IQR instance, smqtk/smqtk_iqr/web/search_app/__init__.py.
@@ -19,7 +19,34 @@ is generated from a manifest file that contains the paths to the image files
 and the corresponding descriptor files. The manifest file is created by the
 chip_images_demo.py script that generates image chips from kwcoco images and
 builds dummy descriptor files.
-"""
+
+
+Requires specific descriptor branch:
+
+    https://github.com/pbeasly/SMQTK-Descriptors.git@dev/prepopulated_descr_generator
+
+Workaround issue by installing specific versions
+
+    pip install flask==2.0.1 werkzeug==2.0.0
+
+Also need:
+    pip install faiss-cpu
+
+Install Mongo:
+    # https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+
+    curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+       sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+       --dearmor
+
+    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+    sudo apt-get install -y mongodb-org
+
+    sudo systemctl start mongod
+
+
+'
 
 # ---------------------------------------------------------------------------
 # Steps to run the IQR demo with a prepopulated descriptor set
@@ -43,9 +70,14 @@ python build_models_demo.py -v \
   -m ../../demodata/manifest.json \
   -t "GEOWATCH_DEMO"
 
+
+# NOTE: depending on versions of mongo version 3.x for Ubuntu 20.04 is the
+# above command and 7.x for Ubuntu 22.04
+mongo_20_04(){
 # 3. Run mongodb service if not already started - config is set to use default
 # host and port ://127.0.0.1:27017
 sudo systemctl start mongodb
+sudo systemctl status mongodb
 
 # 4. check the status of the service
 mongo --eval "db.getMongo()"
@@ -53,23 +85,39 @@ mongo --eval "db.getMongo()"
 # connecting to: mongodb://127.0.0.1:27017
 # 'smqtk' database will be created when the IQR service is run the first time.
 # Enter a mongo shell, to view available databases:
-mongo
-show dbs
+mongo --eval "show dbs"
+
+}
+
+mongo_22_04(){
+# 3. Run mongodb service if not already started - config is set to use default
+# host and port ://127.0.0.1:27017
+sudo systemctl start mongod
+sudo systemctl status mongod
+
+# 4. check the status of the service
+mongosh --eval "db.getMongo()"
+# Should have message:
+# connecting to: mongodb://127.0.0.1:27017
+# 'smqtk' database will be created when the IQR service is run the first time.
+# Enter a mongo shell, to view available databases:
+mongosh --eval "show dbs"
+
+}
+
 
 # 5. Run the IQR search dispatcher and IQR service.
 # This script does not need to be run in the tutorials directory; however,
 # the config file paths need to be provided.
 
 # Following script provides commands to run in two tmux sessions
-COMMAND="runApplication -v -a IqrService \
--c /home/local/KHQ/paul.beasly/code/SMQTK-IQR/smqtk_iqr/docs/tutorials/runApp.IqrRestService.json"
+COMMAND="runApplication -v -a IqrService -c runApp.IqrRestService.json"
 SESSION_ID="SMQTK-IQR-SERVICE"
 tmux kill-session -t "$SESSION_ID" || true
 tmux new-session -d -s "$SESSION_ID" "bash"
 tmux send -t "$SESSION_ID" "$COMMAND" Enter
 
-COMMAND="runApplication -v -a IqrSearchDispatcher \
--c /home/local/KHQ/paul.beasly/code/SMQTK-IQR/smqtk_iqr/docs/tutorials/runApp.IqrSearchApp.json"
+COMMAND="runApplication -v -a IqrSearchDispatcher -c runApp.IqrSearchApp.json"
 SESSION_ID="SMQTK-IQR-SEARCH-DISPATCHER"
 tmux kill-session -t "$SESSION_ID" || true
 tmux new-session -d -s "$SESSION_ID" "bash"
@@ -83,9 +131,7 @@ python -c "import webbrowser; webbrowser.open('127.0.0.1:5000')"
 # 8. Both username and password are set to 'demo' by config files
 
 # Appendix:  If you want to perform step 5 in separate terminals, run:
-runApplication -a IqrService \
--c /home/local/KHQ/paul.beasly/code/SMQTK-IQR/smqtk_iqr/docs/tutorials/runApp.IqrRestService.json
+runApplication -a IqrService -c runApp.IqrRestService.json
 
 # In second terminal run the IQRsearch dispatcher
-runApplication -a IqrSearchDispatcher
--c /home/local/KHQ/paul.beasly/code/SMQTK-IQR/smqtk_iqr/docs/tutorials/runApp.IqrSearchApp.json
+runApplication -a IqrSearchDispatcher -c runApp.IqrSearchApp.json
